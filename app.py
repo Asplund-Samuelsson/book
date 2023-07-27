769c2fd803f935d0e46a6202e1adf348a8ca301b
Johannes Asplund-Samuelsson <johannes.aspsam@gmail.com>
2023-07-28 00:21:35 +0200

    Add login requirement

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import secrets
+import os
 
 from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 from src.book import BookingManager
 
@@ -9,14 +12,59 @@ app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
 
 b = BookingManager()
 
+login = LoginManager(app)
+login.login_view = 'login'
+login.login_message = 'Logga in för att visa sidan.'
+
+
+class User(UserMixin):
+    def check_password(self, password):
+        return password == os.getenv('PASSWORD')
+
+    def get_id(self):
+        return 1
+
+
+user = User()
+
+
+@login.user_loader
+def load_user(user_id):
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        if not user.check_password(request.form['password']):
+            flash('Fel lösenord.')
+            return redirect(url_for('login'))
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html')
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html', bookings=b.index_list())
 
 
 @app.route('/create/', defaults={'booking_id': ''}, methods=('GET', 'POST'))
 @app.route('/create/<booking_id>', methods=('GET', 'POST'))
+@login_required
 def create(booking_id):
     edit = booking_id != ''
     b.set_context(booking_id)
@@ -47,6 +95,7 @@ def create(booking_id):
 
 
 @app.route('/show/<booking_id>')
+@login_required
 def show(booking_id):
     b.set_context(booking_id)
     return render_template('show.html', booking=b.to_table(), booking_id=booking_id)
@@ -54,6 +103,7 @@ def show(booking_id):
 
 @app.route('/answer/<booking_id>', defaults={'edit_name': ''}, methods=['GET', 'POST'])
 @app.route('/answer/<booking_id>/<edit_name>', methods=['GET', 'POST'])
+@login_required
 def answer(booking_id, edit_name):
     edit = edit_name != ''
     b.set_context(booking_id)
@@ -92,6 +142,7 @@ def answer(booking_id, edit_name):
 
 
 @app.route('/comment/<booking_id>', methods=['GET', 'POST'])
+@login_required
 def comment(booking_id):
     b.set_context(booking_id)
 
